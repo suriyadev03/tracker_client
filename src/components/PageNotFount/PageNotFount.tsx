4f4cbbb9ad237003e9a2cb8de6336f11cb26cc96
suriyadev03 <suriyaprakashdev03@gmail.com>
2024-07-04 12:52:19 +0530

    Update PageNotFount.tsx

--- src/components/PageNotFount/PageNotFount.tsx
+++ src/components/PageNotFount/PageNotFount.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@mui/material'
-import Link, { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const PageNotFount = () => {
   const navigate = useNavigate()
